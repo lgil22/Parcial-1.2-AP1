b90d7d47c6bd30eaf8718244b88fca1913f1a0e6
Luis David Gil Baez <luis_gil@ucne.edu.do>
2019-09-25 16:57:29 -0400

    Subiendo Carpeta DAL

---
+++ DAL/Contexto.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.Entity;
+//using Parcial1._2_AP1.Entidades;
+using Parcial1._2_AP1;
+
+namespace Parcial1._2_AP1.DAL
+{
+    public class Contexto
+    {
+        public class Contexto : DbContext
+        {
+            public DbSet<>  { get; set; }
+            
+            public Contexto() : base("ConStr")
+            {
+
+            }
+
+        }
+    }
+
+}
